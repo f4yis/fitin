b284a7d3fbfdbb0f931e4b2f38acbc6974549334
Mohamed Fayis <fayis@outlook.in>
2018-06-26 13:01:27 +0530

    port added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 
+const PORT = process.env.PORT || 3000
 const app = express()
 mongoose.connect('mongodb://fayis:123fayis@ds117681.mlab.com:17681/fitintime')
 let Schema = mongoose.Schema
@@ -34,4 +35,6 @@ app.post('/login', (req, res) => {
 			})
 		})
 })
-app.listen(3000)
+app.listen(PORT, () => {
+	console.log(PORT)
+})
